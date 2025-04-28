c6a4838b46c8191f6289e710104da6ea594190ca
Leonardo <cc24308@g.unicamp.br>
2025-04-28 17:45:59 -0300

    asda

--- fireHelper.py
+++ fireHelper.py
@@ -10,17 +10,17 @@ import json
 
 #asd
 cred_dic = {
-  "type": os.environ["TYPE"],
-  "project_id": os.environ["PROJECT_ID"],
-  "private_key_id": os.environ["PRIVATE_KEY_ID"],
-  "private_key": os.environ["PRIVATE_KEY"].replace("\\n", "\n"), #pode dar erro por causa das quebras de linha
-  "client_email": os.environ["CLIENT_EMAIL"],
-  "client_id": os.environ["CLIENT_ID"],
-  "auth_uri": os.environ["AUTH_URI"],
-  "token_uri": os.environ["TOKEN_URI"],
-  "auth_provider_x509_cert_url": os.environ["AUTH_PROVIDER_X509_CERT_URL"],
-  "client_x509_cert_url": os.environ["CLIENT_X509_CERT_URL"],
-  "universe_domain": os.environ["UNIVERSE_DOMAIN"],
+  "type": os.getenv["TYPE"],
+  "project_id": os.getenv["PROJECT_ID"],
+  "private_key_id": os.getenv["PRIVATE_KEY_ID"],
+  "private_key": os.getenv["PRIVATE_KEY"].replace("\\n", "\n"), #pode dar erro por causa das quebras de linha
+  "client_email": os.getenv["CLIENT_EMAIL"],
+  "client_id": os.getenv["CLIENT_ID"],
+  "auth_uri": os.getenv["AUTH_URI"],
+  "token_uri": os.getenv["TOKEN_URI"],
+  "auth_provider_x509_cert_url": os.getenv["AUTH_PROVIDER_X509_CERT_URL"],
+  "client_x509_cert_url": os.getenv["CLIENT_X509_CERT_URL"],
+  "universe_domain": os.getenv["UNIVERSE_DOMAIN"],
 }
 
 cred = credentials.Certificate(cred_dic)
