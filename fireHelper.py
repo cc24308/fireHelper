04e6d29268a8d610e57ffc68df9b827785d8a9c4
Leonardo <cc24308@g.unicamp.br>
2025-04-28 16:45:34 -0300

    mds ein

--- fireHelper.py
+++ fireHelper.py
@@ -5,7 +5,22 @@ import os
 from dataTypes.Task import Task
 #pip freeze > requirements.txt = pra fazer o txt que o render vai baixar pra fazer a api rodar
 
-cred = credentials.Certificate(os.environ["CREDENTIAL_CERTIFICATE"])
+#cred = credentials.Certificate(os.environ["CREDENTIAL_CERTIFICATE"])
+
+cred = {
+    "type": os.environ["TYPE"],
+  "project_id": os.environ["PROJECT_ID"],
+  "private_key_id": os.environ["PRIVATE_KEY_ID"],
+  "private_key": os.environ["PRIVATE_KEY"].replace("\\n","\n"), #pode dar erro por causa das quebras de linha
+  "client_email": os.environ["CLIENT_EMAIL"],
+  "client_id": os.environ["CLIENT_ID"],
+  "auth_uri": os.environ["AUTH_URI"],
+  "token_uri": os.environ["TOKEN_URI"],
+  "auth_provider_x509_cert_url": os.environ["AUTH_PROVIDER_X509_CERT_URL"],
+  "client_x509_cert_url": os.environ["CLIENT_X509_CERT_URL"],
+  "universe_domain": os.environ["UNIVERSE_DOMAIN"],
+}
+
 firebase_admin.initialize_app(cred)
 
 admin_db = admin_firestore.client()
